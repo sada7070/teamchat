f955c928080fb9946b31b9b950379220ae9793a2
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-02-16 14:10:48 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="">hi there</p>
+      <Button variant="default">Click me</Button>
+    </div>
+  );
+}
